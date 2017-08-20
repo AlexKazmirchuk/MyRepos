926c5d6a1cc689071e22c314221360bf2cd6ca48
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-08-20 10:40:15 +0300

    created GithubService for obtaining user repos and searching repos

---
+++ app/src/main/java/com/alexkaz/myrepos/model/services/GithubService.java
@@ -0,0 +1,47 @@
+package com.alexkaz.myrepos.model.services;
+
+import com.alexkaz.myrepos.model.api.GithubApi;
+import com.alexkaz.myrepos.model.entities.RepoEntity;
+import com.alexkaz.myrepos.model.entities.SearchReposWrapper;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public class GithubService {
+
+    private GithubApi githubApi;
+
+    @Inject
+    public GithubService(GithubApi githubApi) {
+        this.githubApi = githubApi;
+    }
+
+    public Observable<List<RepoEntity>> getUserRepos(){
+        return githubApi.getUserRepos()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    public Observable<List<RepoEntity>> getUserRepos(int page, int perPage){
+        return githubApi.getUserRepos(page, perPage)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    public Observable<SearchReposWrapper> getReposByName(String name){
+        return githubApi.getReposByName(name)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    public Observable<SearchReposWrapper> getReposByName(String name, int page, int perPage){
+        return githubApi.getReposByName(name, page, perPage)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+}
