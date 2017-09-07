6d3756fac457384a2a0a727af667ccc90754e985
AlexKazmirchuk <alexkazmirchuk93@gmail.com>
2017-09-07 18:44:24 +0300

    added Github0AuthApi for getting access token

---
+++ app/src/main/java/com/alexkaz/myrepos/model/entities/TokenEntity.java
@@ -0,0 +1,41 @@
+package com.alexkaz.myrepos.model.entities;
+
+
+import com.google.gson.annotations.SerializedName;
+
+public class TokenEntity {
+
+    @SerializedName("access_token")
+    private String accessToken;
+
+    @SerializedName("token_type")
+    private String tokenType;
+
+    @SerializedName("scope")
+    private String scope;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+}
